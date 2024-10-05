6391f6c6d254671fdb652bebfb35913d15a912d8
Gee1818 <g1sw@hotmail.com>
2024-10-05 11:01:37 -0300

    Added blended prediction on workflow

--- Scripts/utils.py
+++ Scripts/utils.py
@@ -1,34 +1,39 @@
-import numpy as np
-from sklearn.metrics import confusion_matrix
-import plotly.graph_objects as go
-
 from json import loads
+
+import numpy as np
+import pandas as pd
+import plotly.graph_objects as go
+from sklearn.metrics import confusion_matrix
+
 
 def get_artifact_filename(study, prefix):
     for k in study.best_trial.system_attrs.keys():
         artifact = loads(study.best_trial.system_attrs[k])
-        if artifact['filename'][0:len(prefix)]==prefix:
-            return(artifact['artifact_id'])
+        if artifact["filename"][0 : len(prefix)] == prefix:
+            return artifact["artifact_id"]
 
 
-def plot_confusion_matrix(y_test, y_pred, labels=None, title='Conf Matrix', counts=True):
-
+def plot_confusion_matrix(
+    y_test, y_pred, labels=None, title="Conf Matrix", counts=True
+):
     if labels is None:
         labels = np.unique(y_test)
 
-    cm = confusion_matrix(y_test, y_pred, labels = labels, normalize='true')
-    cm_counts = confusion_matrix(y_test, y_pred, labels = labels)
+    cm = confusion_matrix(y_test, y_pred, labels=labels, normalize="true")
+    cm_counts = confusion_matrix(y_test, y_pred, labels=labels)
 
-    hm = go.Heatmap(z=cm*100, y=labels, x=labels)
+    hm = go.Heatmap(z=cm * 100, y=labels, x=labels)
 
     annotations = []
-    for i, row in enumerate(cm):  #iterate through true labels i
-        for j, value in enumerate(row): # iterate through predictions
+    for i, row in enumerate(cm):  # iterate through true labels i
+        for j, value in enumerate(row):  # iterate through predictions
             if counts:
-                ann_text = str(np.round(value*100,1)) + ' - ' + str(round(cm_counts[i,j]))
+                ann_text = (
+                    str(np.round(value * 100, 1)) + " - " + str(round(cm_counts[i, j]))
+                )
             else:
-                ann_text = str(np.round(value*100,1)) 
-                    
+                ann_text = str(np.round(value * 100, 1))
+
             annotations.append(
                 {
                     "x": labels[j],
@@ -37,16 +42,19 @@ def plot_confusion_matrix(y_test, y_pred, labels=None, title='Conf Matrix', coun
                     "text": ann_text,
                     "xref": "x1",
                     "yref": "y1",
-                    "showarrow": False
+                    "showarrow": False,
                 }
             )
     layout = {
         "title": title,
         "xaxis": {"title": "Predicted value"},
         "yaxis": {"title": "Real value"},
-        'width':800,
-        'height':800,        
-        "annotations": annotations
+        "width": 800,
+        "height": 800,
+        "annotations": annotations,
     }
-    fig = go.Figure(data=hm, layout=layout, )
+    fig = go.Figure(
+        data=hm,
+        layout=layout,
+    )
     return fig
